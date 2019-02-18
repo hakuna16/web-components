3f6936e4c842cfee88c3ac86371362625850e506
Rituj Kumar <rituj.kumar@tesco.com>
2019-02-18 20:37:53 +0530

    Adding docs

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** A super lame component that says Hello with a custom message. */
+const HelloWorld = ({message}) => {
+    return(
+        <div>{message}</div>
+    );
+}
+
+HelloWorld.propTypes = {
+    /** Message to display */
+    message: PropTypes.string
+  };
+  
+  HelloWorld.defaultProps = {
+    message: 'World'
+  };
+export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from 'web-component/HelloWorld';
+
+/** Custom Message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Hello To first Docs!!" />
+}
+ 
